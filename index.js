21b9e9a571ffdfb2d45ec4a70bfb58a6d446903b
Daniel Gitau <gitaud959@gmail.com>
2022-11-27 22:25:15 +0300

    Completd

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+//main.remove();
+document.querySelector("#main").remove()
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = 'Sharon is the champion';
+
